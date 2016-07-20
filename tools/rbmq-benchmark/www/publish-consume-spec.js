4085f73e71f0918878531f3db7086136e88ebefa
Alexey Lebedeff <alebedev@mirantis.com>
2016-07-20 20:58:15 +0300

    Cluster rabbits with autocluster plugin
    `etcd` is used as a backend for `autocluster`.
    
    Added by kproskurin:
    
    - Back to binary rbmq
    - Updated commits for autocluster and it's deps
    - Enabled managment plugin, for api checks(adn monitoring in future)
    - Added readiness script, to make sure pod is Ready only after
      it joined a cluster
    - Added container with a rbmq bench tools
    
    Open questions:
    - Demonsets should be used to prevent running more than one rabbit
      instance on single node.
    
    - Erlang should be at least 18.3.1 to avoid some bugs in clustered mode,
      so 18.3.4 from erlang solutions is used. Maybe it should be backported
      from debian unstable instead.
    
    - rabbitmq and autocluster plugin are built from source, as no packages
      exists:
      - for rabbit with commits up to
        https://github.com/rabbitmq/rabbitmq-server/pull/892
      - at all for autocluster
    
    - `/usr/bin/rabbitmq-server` is a wrapper not suitable for running
      rabbit in our case, and `/usr/lib/rabbitmq/bin/rabbitmq-server` is
      broken in official rabbitmq package. So custom start script is used for
      starting using:
    
        /usr/lib/rabbitmq/lib/rabbitmq_server-*/sbin/rabbitmq-server
    
    Change-Id: I98bde66da4be517fb9e4442906620fae536d4f37

---
+++ tools/rbmq-benchmark/www/publish-consume-spec.js
@@ -0,0 +1,6 @@
+[{'name':      'consume',
+  'type':      'simple',
+  'uri':       'amqp://rabbitmq:password@rabbitmq',
+  'params':    [{'time-limit':     30,
+                 'producer-count': 4,
+                 'consumer-count': 2}]}]
